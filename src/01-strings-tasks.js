04550125d12506d9fcfb666e6bd77b30d56b3fa0
Aliaksandr <51595644+ARomanowsky@users.noreply.github.com>
2020-12-14 21:16:56 +0300

    fix: update string

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -1,190 +1,294 @@
-/* *********************************************************************************************
- *                                                                                             *
- * Plese read the following tutorial before implementing tasks:                                *
- * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Functions                     *
- * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function   *
- * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/arguments       *
- * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Closures                            *
- *                                                                                             *
- ********************************************************************************************* */
+/* *******************************************************************************************
+ *                                                                                           *
+ * Plese read the following tutorial before implementing tasks:                              *
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String   *
+ *                                                                                           *
+ ******************************************************************************************* */
 
 
 /**
- * Returns the functions composition of two specified functions f(x) and g(x).
- * The result of compose is to be a function of one argument, (lets call the argument x),
- * which works like applying function f to the result of applying function g to x, i.e.
- *  getComposition(f,g)(x) = f(g(x))
+ * Returns the result of concatenation of two strings.
  *
- * @param {Function} f
- * @param {Function} g
- * @return {Function}
+ * @param {string} value1
+ * @param {string} value2
+ * @return {string}
  *
  * @example
- *   getComposition(Math.sin, Math.asin)(x) => Math.sin(Math.asin(x))
- *
+ *   'aa', 'bb' => 'aabb'
+ *   'aa',''    => 'aa'
+ *   '',  'bb'  => 'bb'
  */
-function getComposition(f, g) {
-  return function combined(x) {
-    return f(g(x));
-  };
+function concatenateStrings(value1, value2) {
+  return value1 + value2;
+}
+
+/**
+ * Returns the length of given string.
+ *
+ * @param {string} value
+ * @return {number}
+ *
+ * @example
+ *   'aaaaa' => 5
+ *   'b'     => 1
+ *   ''      => 0
+ */
+function getStringLength(value) {
+  return value.length;
+}
+
+/**
+ * Returns the result of string template and given parameters firstName and lastName.
+ * Please do not use concatenation, use template string :
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/template_strings
+ *
+ * @param {string} firstName
+ * @param {string} lastName
+ * @return {string}
+ *
+ * @example
+ *   'John','Doe'      => 'Hello, John Doe!'
+ *   'Chuck','Norris'  => 'Hello, Chuck Norris!'
+ */
+function getStringFromTemplate(firstName, lastName) {
+  return `Hello, ${`${firstName} ${lastName}`}!`;
+}
+
+/**
+ * Extracts a name from template string 'Hello, First_Name Last_Name!'.
+ *
+ * @param {string} value
+ * @return {string}
+ *
+ * @example
+ *   'Hello, John Doe!' => 'John Doe'
+ *   'Hello, Chuck Norris!' => 'Chuck Norris'
+ */
+function extractNameFromTemplate(value) {
+  const name = value.split(', ');
+  return name[1].substring(0, name[1].length - 1);
+}
+
+/**
+ * Returns a first char of the given string.
+ *
+ * @param {string} value
+ * @return {string}
+ *
+ * @example
+ *   'John Doe'  => 'J'
+ *   'cat'       => 'c'
+ */
+function getFirstChar(value) {
+  return value.charAt(0);
+}
+
+/**
+ * Removes a leading and trailing whitespace characters from string.
+ *
+ * @param {string} value
+ * @return {string}
+ *
+ * @example
+ *   '  Abracadabra'    => 'Abracadabra'
+ *   'cat'              => 'cat'
+ *   '\tHello, World! ' => 'Hello, World!'
+ */
+function removeLeadingAndTrailingWhitespaces(value) {
+  return value.trim();
+}
+
+/**
+ * Returns a string that repeated the specified number of times.
+ *
+ * @param {string} value
+ * @param {string} count
+ * @return {string}
+ *
+ * @example
+ *   'A', 5  => 'AAAAA'
+ *   'cat', 3 => 'catcatcat'
+ */
+function repeatString(value, count) {
+  return value.repeat(count);
+}
+
+/**
+ * Remove the first occurrence of string inside another string
+ *
+ * @param {string} str
+ * @param {string} value
+ * @return {string}
+ *
+ * @example
+ *   'To be or not to be', 'not'  => 'To be or  to be'
+ *   'I like legends', 'end' => 'I like legs',
+ *   'ABABAB','BA' => 'ABAB'
+ */
+function removeFirstOccurrences(str, value) {
+  return str.replace(value, '');
+}
+
+/**
+ * Remove the first and last angle brackets from tag string
+ *
+ * @param {string} str
+ * @return {string}
+ *
+ * @example
+ *   '<div>' => 'div'
+ *   '<span>' => 'span'
+ *   '<a>' => 'a'
+ */
+function unbracketTag(str) {
+  return str.slice(1, -1);
 }
 
 
 /**
- * Returns the math power function with the specified exponent
+ * Converts all characters of the specified string into the upper case
  *
- * @param {number} exponent
- * @return {Function}
+ * @param {string} str
+ * @return {string}
  *
  * @example
- *   const power2 = getPowerFunction(2); // => x^2
- *   power2(2) => 4
- *   power2(4) => 16
- *
- *   const power05 = getPowerFunction(0.5); // => x^0.5
- *   power05(4) => 2
- *   power05(16) => 4
- *
+ *   'Thunderstruck' => 'THUNDERSTRUCK'
+ *  'abcdefghijklmnopqrstuvwxyz' => 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
  */
-function getPowerFunction(exponent) {
-  return function power(x) {
-    return x ** exponent;
-  };
+function convertToUpperCase(str) {
+  return str.toUpperCase();
 }
 
-
 /**
- * Returns the polynom function of one argument based on specified coefficients.
- * See: https://en.wikipedia.org/wiki/Polynomial#Definition
+ * Extracts e-mails from single string with e-mails list delimeted by semicolons
  *
- * @params {integer}
- * @return {Function}
+ * @param {string} str
+ * @return {array}
  *
  * @example
- *   getPolynom(2,3,5) => y = 2*x^2 + 3*x + 5
- *   getPolynom(1,-3)  => y = x - 3
- *   getPolynom(8)     => y = 8
- *   getPolynom()      => null
+ *   'angus.young@gmail.com;brian.johnson@hotmail.com;bon.scott@yahoo.com'
+ *   => [
+ *      'angus.young@gmail.com',
+ *      'brian.johnson@hotmail.com',
+ *      'bon.scott@yahoo.com'
+ *   ],
+ *   'info@gmail.com' => ['info@gmail.com']
  */
-function getPolynom() {
+function extractEmails(str) {
+  return str.split(';');
+}
+
+/**
+ * Returns the string representation of rectangle with specified width and height
+ * using pseudograhic chars
+ *
+ * @param {number} width
+ * @param {number} height
+ * @return {string}
+ *
+ * @example
+ *
+ *            '┌────┐\n'+
+ *  (6,4) =>  '│    │\n'+
+ *            '│    │\n'+
+ *            '└────┘\n'
+ *
+ *  (2,2) =>  '┌┐\n'+
+ *            '└┘\n'
+ *
+ *             '┌──────────┐\n'+
+ *  (12,3) =>  '│          │\n'+
+ *             '└──────────┘\n'
+ *
+ */
+function getRectangleString(/* width, height */) {
   throw new Error('Not implemented');
 }
 
 
 /**
- * Memoizes passed function and returns function
- * which invoked first time calls the passed function and then always returns cached result.
+ * Encode specified string with ROT13 cipher
+ * See details:  https://en.wikipedia.org/wiki/ROT13
  *
- * @params {Function} func - function to memoize
- * @return {Function} memoized function
+ * @param {string} str
+ * @return {string}
  *
  * @example
- *   const memoizer = memoize(() => Math.random());
- *   memoizer() => some random number  (first run, evaluates the result of Math.random())
- *   memoizer() => the same random number  (second run, returns the previous cached result)
- *   ...
- *   memoizer() => the same random number  (next run, returns the previous cached result)
+ *
+ *   'hello' => 'uryyb'
+ *   'Why did the chicken cross the road?' => 'Jul qvq gur puvpxra pebff gur ebnq?'
+ *   'Gb trg gb gur bgure fvqr!' => 'To get to the other side!'
+ *   'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+ *    => 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm'
+ *
  */
-function memoize(/* func */) {
+function encodeToRot13(/* str */) {
   throw new Error('Not implemented');
+}
+
+/**
+ * Returns true if the value is string; otherwise false.
+ * @param {string} value
+ * @return {boolean}
+ *
+ * @example
+ *   isString() => false
+ *   isString(null) => false
+ *   isString([]) => false
+ *   isString({}) => false
+ *   isString('test') => true
+ *   isString(new String('test')) => true
+ */
+function isString(value) {
+  return typeof value === 'string' || value instanceof String;
 }
 
 
 /**
- * Returns the function trying to call the passed function and if it throws,
- * retrying it specified number of attempts.
+ * Returns playid card id.
  *
- * @param {Function} func
- * @param {number} attempts
- * @return {Function}
+ * Playing cards inittial deck inclides the cards in the following order:
  *
- * @example
- * const attempt = 0, retryer = retry(() => {
- *      if (++attempt % 2) throw new Error('test');
- *      else return attempt;
- * }, 2);
- * retryer() => 2
- */
-function retry(/* func, attempts */) {
-  throw new Error('Not implemented');
-}
-
-
-/**
- * Returns the logging wrapper for the specified method,
- * Logger has to log the start and end of calling the specified function.
- * Logger has to log the arguments of invoked function.
- * The fromat of output log is:
- * <function name>(<arg1>, <arg2>,...,<argN>) starts
- * <function name>(<arg1>, <arg2>,...,<argN>) ends
+ *  'A♣','2♣','3♣','4♣','5♣','6♣','7♣','8♣','9♣','10♣','J♣','Q♣','K♣',
+ *  'A♦','2♦','3♦','4♦','5♦','6♦','7♦','8♦','9♦','10♦','J♦','Q♦','K♦',
+ *  'A♥','2♥','3♥','4♥','5♥','6♥','7♥','8♥','9♥','10♥','J♥','Q♥','K♥',
+ *  'A♠','2♠','3♠','4♠','5♠','6♠','7♠','8♠','9♠','10♠','J♠','Q♠','K♠'
  *
+ * (see https://en.wikipedia.org/wiki/Standard_52-card_deck)
+ * Function returns the zero-based index of specified card in the initial deck above.
  *
- * @param {Function} func
- * @param {Function} logFunc - function to output log with single string argument
- * @return {Function}
+ * @param {string} value
+ * @return {number}
  *
  * @example
- *
- * const cosLogger = logger(Math.cos, console.log);
- * const result = cosLogger(Math.PI));     // -1
- *
- * log from console.log:
- * cos(3.141592653589793) starts
- * cos(3.141592653589793) ends
- *
+ *   'A♣' => 0
+ *   '2♣' => 1
+ *   '3♣' => 2
+ *     ...
+ *   'Q♠' => 50
+ *   'K♠' => 51
  */
-function logger(/* func, logFunc */) {
-  throw new Error('Not implemented');
-}
-
-
-/**
- * Return the function with partial applied arguments
- *
- * @param {Function} fn
- * @return {Function}
- *
- * @example
- *   const fn = function(x1,x2,x3,x4) { return  x1 + x2 + x3 + x4; };
- *   partialUsingArguments(fn, 'a')('b','c','d') => 'abcd'
- *   partialUsingArguments(fn, 'a','b')('c','d') => 'abcd'
- *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
- *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
- */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
-}
-
-
-/**
- * Returns the id generator function that returns next integer starting
- * from specified number every time when invoking.
- *
- * @param {Number} startFrom
- * @return {Function}
- *
- * @example
- *   const getId4 = getIdGenerator(4);
- *   const getId10 = gerIdGenerator(10);
- *   getId4() => 4
- *   getId10() => 10
- *   getId4() => 5
- *   getId4() => 6
- *   getId4() => 7
- *   getId10() => 11
- */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getCardId(value) {
+  const arr = ['A♣', '2♣', '3♣', '4♣', '5♣', '6♣', '7♣', '8♣', '9♣', '10♣', 'J♣', 'Q♣', 'K♣', 'A♦', '2♦', '3♦', '4♦', '5♦', '6♦', '7♦', '8♦', '9♦', '10♦', 'J♦', 'Q♦', 'K♦', 'A♥', '2♥', '3♥', '4♥', '5♥', '6♥', '7♥', '8♥', '9♥', '10♥', 'J♥', 'Q♥', 'K♥', 'A♠', '2♠', '3♠', '4♠', '5♠', '6♠', '7♠', '8♠', '9♠', '10♠', 'J♠', 'Q♠', 'K♠'];
+  return arr.indexOf(value);
 }
 
 
 module.exports = {
-  getComposition,
-  getPowerFunction,
-  getPolynom,
-  memoize,
-  retry,
-  logger,
-  partialUsingArguments,
-  getIdGeneratorFunction,
+  concatenateStrings,
+  getStringLength,
+  getStringFromTemplate,
+  extractNameFromTemplate,
+  getFirstChar,
+  removeLeadingAndTrailingWhitespaces,
+  repeatString,
+  removeFirstOccurrences,
+  unbracketTag,
+  convertToUpperCase,
+  extractEmails,
+  getRectangleString,
+  encodeToRot13,
+  isString,
+  getCardId,
 };
+
